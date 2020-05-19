79014aac22dc2d3922ab1325a823a89852a5cf4e
WayfaingStranger <shubhampaitrik8@gmail.com>
2020-05-19 20:11:13 +0530

    store.html

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),
-    path('cart/', views.cart, name='card'),
+    path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout')
 ]
