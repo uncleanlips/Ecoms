50d660343ed9337e4afdb29d3820df161fe01c12
uncleanlips <holyscripturesareable@gmail.com>
2020-05-20 08:12:44 +0530

    Models

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Customer
+from .models import Product
+from .models import Order
+from .models import OrderItem
+from .models import ShippingAddress
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
